5ba6e2ce881990b8ed30b6a8760b04f9b733efa9
Mohamed Ali <mohammed.ali17955@gmail.com>
2024-08-19 07:33:45 +0300

    Adding 1-simple_pagination.py and 1-main.py files

---
+++ 0x00-pagination/1-simple_pagination.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+"""This module contains index_range() function and Server class"""
+
+import csv
+import math
+from typing import List, Tuple
+import csv
+
+
+def index_range(page, page_size) -> Tuple[int, int]:
+    """
+    Returns a tuple of size two containing a start index and an end index
+    corresponding to the range of indexes to return in a list for those
+    particular pagination parameters.
+    """
+
+    end_index = page_size * page
+    start_index = end_index - page_size
+
+    return (start_index, end_index)
+
+
+class Server:
+    """Server class to paginate a database of popular baby names."""
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        assert type(page) == int and page > 0
+        assert type(page_size) == int and page_size > 0
+        with open ('Popular_Baby_Names.csv' ,'r') as file:
+            csv_reader = csv.reader(file)
+            result = index_range(page, page_size)
+            return list(csv_reader)[result[0]: result[1]]
