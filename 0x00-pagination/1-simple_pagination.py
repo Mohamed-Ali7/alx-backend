29b38fecc6132043a3df414e5570086617c11aa4
Mohamed Ali <mohammed.ali17955@gmail.com>
2024-08-19 09:38:59 +0300

    Updating 1-simple_pagination.py file

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -1,18 +1,29 @@
 #!/usr/bin/env python3
-"""
-Adds `get_page` method to `Server` class
-"""
-import csv
+"""This module contains index_range() function and Server class"""
+
 from typing import List, Tuple
+import csv
 
 
 class Server:
-    """Server class to paginate a database of popular baby names.
-    """
+    """Server class to paginate a database of popular baby names."""
     DATA_FILE = "Popular_Baby_Names.csv"
 
     def __init__(self):
         self.__dataset = None
+
+    @staticmethod
+    def index_range(page, page_size) -> Tuple[int, int]:
+        """
+        Returns a tuple of size two containing a start index and an end index
+        corresponding to the range of indexes to return in a list for those
+        particular pagination parameters.
+        """
+
+        end_index = page_size * page
+        start_index = end_index - page_size
+
+        return (start_index, end_index)
 
     def dataset(self) -> List[List]:
         """Cached dataset
@@ -25,24 +36,11 @@ class Server:
 
         return self.__dataset
 
-    @staticmethod
-    def index_range(page: int, page_size: int) -> Tuple[int, int]:
-        """Calculate start and end index range for a `page`, with `page_size`
-        """
-        nextPageStartIndex = page * page_size
-        return nextPageStartIndex - page_size, nextPageStartIndex
-
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-        """
-        Get items for the given page number
-        Args:
-            page (int): page number
-            page_size (int): number of items per page
-        Returns:
-            (List[List]): a list of list(row) if inputs are within range
-            ([]) : an empty list if page and page_size are out of range
-        """
-        assert type(page) == int and type(page_size) == int
+        assert type(page) is int and type(page_size) is int
         assert page > 0 and page_size > 0
-        startIndex, endIndex = self.index_range(page, page_size)
-        return self.dataset()[startIndex:endIndex]
+        start_index, end_index = self.index_range(page, page_size)
+        dataset = self.dataset()
+        if start_index > len(dataset):
+            return []
+        return dataset[start_index: end_index]
