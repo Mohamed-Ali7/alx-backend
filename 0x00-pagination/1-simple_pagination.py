8d3d2f604ece44491e8772194c6a296c81f7a69b
Mohamed Ali <mohammed.ali17955@gmail.com>
2024-08-19 09:27:20 +0300

    Updating 1-simple_pagination.py file

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -1,23 +1,14 @@
 #!/usr/bin/env python3
-"""This module contains index_range() function and Server class"""
-
-import math
-from typing import List, Tuple
+"""
+Adds `get_page` method to `Server` class
+"""
 import csv
-
-
-def index_range(page, page_size) -> Tuple[int, int]:
-    """
-    Returns a tuple of size two containing a start index and an end index
-    corresponding to the range of indexes to return in a list for those
-    particular pagination parameters.
-    """
-
-    return ((page - 1) * page_size, ((page - 1) * page_size) + page_size)
+from typing import List, Tuple
 
 
 class Server:
-    """Server class to paginate a database of popular baby names."""
+    """Server class to paginate a database of popular baby names.
+    """
     DATA_FILE = "Popular_Baby_Names.csv"
 
     def __init__(self):
@@ -34,11 +25,24 @@ class Server:
 
         return self.__dataset
 
+    @staticmethod
+    def index_range(page: int, page_size: int) -> Tuple[int, int]:
+        """Calculate start and end index range for a `page`, with `page_size`
+        """
+        nextPageStartIndex = page * page_size
+        return nextPageStartIndex - page_size, nextPageStartIndex
+
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """
+        Get items for the given page number
+        Args:
+            page (int): page number
+            page_size (int): number of items per page
+        Returns:
+            (List[List]): a list of list(row) if inputs are within range
+            ([]) : an empty list if page and page_size are out of range
+        """
         assert type(page) == int and type(page_size) == int
         assert page > 0 and page_size > 0
-        start, end = index_range(page, page_size)
-        data = self.dataset()
-        if start > len(data):
-            return []
-        return data[start:end]
+        startIndex, endIndex = self.index_range(page, page_size)
+        return self.dataset()[startIndex:endIndex]
