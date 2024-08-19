59b017641ce9ed229091b4581f4621d9e209d8b2
Mohamed Ali <mohammed.ali17955@gmail.com>
2024-08-19 08:18:55 +0300

    Updating 1-simple_pagination.py file

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -13,8 +13,9 @@ def index_range(page, page_size) -> Tuple[int, int]:
     particular pagination parameters.
     """
 
-    end_index = page_size * page
-    start_index = end_index - page_size
+    start_index = (page - 1) * page_size
+    end_index = start_index + page_size
+
 
     return (start_index, end_index)
 
